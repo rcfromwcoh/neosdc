872defd1c8ecf1c89f55cd3767704cff6fc81b5b
Robert Horrocks <rhorrocks@bds.local>
2016-09-19 11:57:56 -0400

    first commit

---
+++ neosdcv1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(neosdcv1.Startup))]
+namespace neosdcv1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
